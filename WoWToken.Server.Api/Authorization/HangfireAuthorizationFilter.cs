ef04d3588a606bafff5caca09ebbf54da02559fc
Cedric Evrard <cedric@evrard.io>
2020-06-06 23:14:49 +0200

    temporary update dashboard access

---
+++ WoWToken.Server.Api/Authorization/HangfireAuthorizationFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace WoWToken.Server.Api.Authorization
+{
+    public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
